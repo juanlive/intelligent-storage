a817bd0120ed9247d930c8f9a1ddf0d498e38ecf
Juan Livingston <juanlivingston@gmail.com>
2018-03-16 10:14:32 -0300

    Update GlobalStorageMultiId.sol

--- GlobalStorageMultiId.sol
+++ GlobalStorageMultiId.sol
@@ -1,42 +1,21 @@
 pragma solidity ^0.4.15;
 
-
-/*
-
-Global storage for multiple users. Multiple accounts per address.
-
-- Controlled by Id (many ids per address)
-- Updateable address (address can be replaced by a new one with the same permission to access data)
-
-
-Details:
-
-Data is related to a specific Id. Only one address is authorized to read and write data for each Id. 
-Each address can transfer its ownership of an Id to another address.
-
-Usage:
-
-registerUser(_id): registers a new Id to the caller address.
-
-changeAddress(_id , _address): transfers the Id of the current address to a new one.
-
-checkId(_id): get the address associated with the specified Id.
-
-setUint(_id , _key , _data , _overwrite): stores a uint with a given key. Key may be an address or a string. It is encoded as bytes32. 
-_overwrite is an optional boolean. If it is specified as "true", data may overwrite any previous data. If it is not specified and 
-there is already data stored for these key, it will return false with an error log.
-
-getUint(_id , _key): retrieves the data stored for that key.
-
-setBytes32, setString, setAddress, getBytes32, getString, getAddress: same as setUint and getUint
-
-*/
+// Global Storage Multi id
+// Author: Juan Livingston @ Ethernity.live
 
 contract GlobalStorageMultiId {
     
-    mapping (bytes32 => address) users;
-    // mapping (address => bytes32) ids;
     uint256 public totalUsers;
+    uint256 public regPrice;
+    uint256 public totalCollected;
+    address public admin;
+
+    // mapping(address => bytes32) ids;
+    mapping(bytes32 => address) users;
+    mapping(bytes32 => mapping(bytes32 => uint256)) dataUint;
+    mapping(bytes32 => mapping(bytes32 => bytes32)) dataBytes32;
+    mapping(bytes32 => mapping(bytes32 => string)) dataString;
+    mapping(bytes32 => mapping(bytes32 => address)) dataAddress; 
 
     event Error(string _string);
     event RegisteredUser(address _address , bytes32 _id);
@@ -44,28 +23,44 @@ contract GlobalStorageMultiId {
 
 
     modifier onlyOwner(bytes32 _id) {
-        if ( users[_id] != msg.sender ) {
-            revert();
-        }
+        require(msg.sender == users[_id]);
         _;
     }
 
-    function registerUser(bytes32 _id) returns(bool) {
+    modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
+
+
+    function GlobalStorageMultiId() {
+        regPrice = 0.005 ether; // Promotional price - will change in the future
+        admin = msg.sender;
+    }
+
+
+    // User's admin functions
+
+    function registerUser(bytes32 _id) payable returns(bool) {
+
+        require(msg.value >= regPrice);
+
         if ( users[_id] != 0x0 ) {
-            Error("User already exists");
+            Error("ID already exists");
+            msg.sender.send(msg.value);
             return false;
         }
-        if (_id == 0x0) {
-            Error("Invalid Id");
-            return false;
-        }
+
         users[_id] = msg.sender;
         // ids[msg.sender] = _id;
-        totalUsers = totalUsers + 1;
+        totalUsers += 1;
+        totalCollected += msg.value;
+        admin.send(msg.value);
         RegisteredUser(msg.sender , _id);
         return true;
     }
     
+
     function changeAddress(bytes32 _id , address _newAddress) onlyOwner(_id) returns(bool) {
         users[_id] = _newAddress;
         ChangedAdd(_id , msg.sender , _newAddress);
@@ -75,18 +70,14 @@ contract GlobalStorageMultiId {
     function checkId(bytes32 _id) constant returns(address _address) {
         return users[_id];
     }
-    
-    
-    mapping(bytes32 => mapping(bytes32 => uint256)) dataUint;
-    mapping(bytes32 => mapping(bytes32 => bytes32)) dataBytes32;
-    mapping(bytes32 => mapping(bytes32 => string)) dataString;
-    mapping(bytes32 => mapping(bytes32 => address)) dataAddress; 
 
 
-    // Uint type
+    // Users's data storage
+
+    // Uint
 
     function setUint(bytes32 _id , bytes32 _key , uint256 _data , bool _overwrite) onlyOwner(_id) returns(bool) {
-        if ( _overwrite || dataUint[_id][_key] == 0 ) {
+        if (dataUint[_id][_key] == 0 ||  _overwrite) {
             dataUint[_id][_key] = _data;
             return true;
         } else {
@@ -100,10 +91,10 @@ contract GlobalStorageMultiId {
     }
 
 
-    // String type
+    // String
 
     function setString(bytes32 _id , bytes32 _key , string _data , bool _overwrite) onlyOwner(_id) returns(bool) {
-        if ( _overwrite || bytes(dataString[_id][_key]).length == 0 ) {
+        if (bytes(dataString[_id][_key]).length == 0  ||  _overwrite) {
             dataString[_id][_key] = _data;
             return true;
         } else {
@@ -116,10 +107,10 @@ contract GlobalStorageMultiId {
         return dataString[_id][_key];
     }
 
-    // Address type
+    // Address
 
     function setAddress(bytes32 _id , bytes32 _key , address _data , bool _overwrite) onlyOwner(_id) returns(bool) {
-        if ( _overwrite || dataAddress[_id][_key] == 0x0 ) {
+        if (dataAddress[_id][_key] == 0x0  ||  _overwrite) {
             dataAddress[_id][_key] = _data;
             return true;
         } else {
@@ -132,10 +123,10 @@ contract GlobalStorageMultiId {
         return dataAddress[_id][_key];
     }
 
-    // Bytes32 type
+    // Bytes32
     
     function setBytes32(bytes32 _id , bytes32 _key , bytes32 _data , bool _overwrite) onlyOwner(_id) returns(bool) {
-        if ( _overwrite || dataBytes32[_id][_key] == 0x0 ) {
+        if (dataBytes32[_id][_key] == 0x0  ||  _overwrite) {
             dataBytes32[_id][_key] = _data;
             return true;
         } else {
@@ -147,4 +138,16 @@ contract GlobalStorageMultiId {
     function getBytes32(bytes32 _id , bytes32 _key) constant returns(bytes32 _data) {
         return dataBytes32[_id][_key];
     }
+
+
+    // Admin functions
+
+    function changePrice(uint _newPrice) onlyAdmin {
+        regPrice = _newPrice;
+    }
+
+    function changeAdmin(address _newAdmin) onlyAdmin {
+        admin = _newAdmin;
+    }
+
 }
